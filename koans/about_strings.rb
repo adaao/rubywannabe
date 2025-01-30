97e85189b90645119c43fed665d7b6c8f1c1004b
Adaão Ribeiro Mascarelli <armascarelli@gmail.com>
2025-01-30 22:54:33 +0000

    test_strings_can_be_split

--- koans/about_strings.rb
+++ koans/about_strings.rb
@@ -145,31 +145,32 @@ EOS
 
   def test_you_can_get_a_single_character_from_a_string
     string = "Bacon, lettuce and tomato"
-    assert_equal __, string[1]
+    assert_equal 'a', string[1]
 
     # Surprised?
+    # no!
   end
 
   in_ruby_version("1.8") do
     def test_in_older_ruby_single_characters_are_represented_by_integers
-      assert_equal __, ?a
-      assert_equal __, ?a == 97
+      assert_equal 97, ?a
+      assert_equal true, ?a == 97
 
-      assert_equal __, ?b == (?a + 1)
+      assert_equal true, ?b == (?a + 1)
     end
   end
 
   in_ruby_version("1.9", "2", "3") do
     def test_in_modern_ruby_single_characters_are_represented_by_strings
-      assert_equal __, ?a
-      assert_equal __, ?a == 97
+      assert_equal "a", ?a
+      assert_equal false, ?a == 97
     end
   end
 
   def test_strings_can_be_split
     string = "Sausage Egg Cheese"
     words = string.split
-    assert_equal [__, __, __], words
+    assert_equal ['Sausage', "Egg", "Cheese"], words
   end
 
   def test_strings_can_be_split_with_different_patterns
